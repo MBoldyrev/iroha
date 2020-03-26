a108e918cb64da2e513bf1129a8f0b7feee3063f
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-03-26 16:58:56 -0500

    logger uses iroha::to_string
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- libs/logger/logger.hpp
+++ libs/logger/logger.hpp
@@ -16,16 +16,47 @@
 // struct, leading to compilation issues
 #undef interface
 
+#include "common/to_string.hpp"
+
+namespace iroha {
+  namespace utility_service {
+    namespace proto {
+      class Status;
+    }
+  }  // namespace utility_service
+}  // namespace iroha
+
+namespace logger {
+  namespace detail {
+    template <typename T>
+    class FmtSupportTypeChecker {
+     private:
+      typedef char YesType[1];
+      typedef char NoType[2];
+
+      template <typename C>
+      static YesType &test(decltype(
+          std::declval<::fmt::internal::arg_mapper<::fmt::format_context>>()
+              .map(std::declval<C>())));
+      template <typename C>
+      static NoType &test(...);
+
+     public:
+      enum { value = sizeof(test<T>(0)) == sizeof(YesType) };
+    };
+  }  // namespace detail
+  template <typename T>
+  constexpr bool kIsFmtInternalSupported =  // true;
+      detail::FmtSupportTypeChecker<T>::value;
+}  // namespace logger
+
 namespace fmt {
-  /// Allows to log objects, which have toString() method without calling it,
-  /// e.g. log.info("{}", myObject)
+  /// Allows to directly log objects, which are not supported by libfmt
+  /// natively, using iroha::to_string
   template <typename T>
   struct formatter<
       T,
-      std::enable_if_t<std::is_same<decltype(std::declval<T>().toString()),
-                                    std::string>::value,
-                       char>> {
-    // The following functions are not defined intentionally.
+      std::enable_if_t<!::logger::kIsFmtInternalSupported<T>, char>> {
     template <typename ParseContext>
     auto parse(ParseContext &ctx) -> decltype(ctx.begin()) {
       return ctx.begin();
@@ -33,7 +64,7 @@ namespace fmt {
 
     template <typename FormatContext>
     auto format(const T &val, FormatContext &ctx) -> decltype(ctx.out()) {
-      return format_to(ctx.out(), "{}", val.toString());
+      return format_to(ctx.out(), "{}", ::iroha::to_string::toString(val));
     }
   };
 }  // namespace fmt
