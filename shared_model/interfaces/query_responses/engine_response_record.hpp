6de25c87f3c21b71eecb4c5d3e6c09767b6781de
iceseer <iceseer@gmail.com>
2020-05-01 17:13:01 +0300

    interface

--- shared_model/interfaces/query_responses/engine_response_record.hpp
+++ shared_model/interfaces/query_responses/engine_response_record.hpp
@@ -16,12 +16,40 @@ namespace shared_model {
     /// Provides a response for a single corresponding EngineCall command
     class EngineReceipt : public ModelPrimitive<EngineReceipt> {
      public:
+
+      enum struct PayloadType {
+        kPayloadTypeTo,
+        kPayloadTypeContractAddress
+      };
+
+      using EngineLogsCollectionType = std::vector<interface::EngineLog>;
+
       /// Get the index
       virtual interface::types::CommandIndexType commandIndex() const = 0;
 
-      /// Get the response
-      virtual const interface::types::SmartContractCodeType &response()
-          const = 0;
+      /// Transaction hash
+      types::HashType getTxHash() const = 0;
+
+      /// Transaction index
+      types::TxIndexType getTxIndex() const = 0;
+
+      /// Block height
+      types::HeightType getBlockHeight() const = 0;
+
+      /// Block hash
+      types::HashType getBlockHash() const = 0;
+
+      /// Get sender account id
+      types::AccountIdType getFrom() const = 0;
+
+      /// Returns the payload data type.
+      PayloadType getPayloadType() const = 0;
+
+      /// Returns payload data
+      EvmAddressHexString getPayload() const = 0;
+
+      /// Return engine logs collection.
+      EngineLogsCollectionType const &getEngineLogs() const = 0;
 
       std::string toString() const override;
 
--- shared_model/interfaces/query_responses/impl/engine_response_record.cpp
+++ shared_model/interfaces/query_responses/impl/engine_response_record.cpp
@@ -7,14 +7,33 @@
 
 using namespace shared_model::interface;
 
-bool EngineReceipt::operator==(const ModelType &rhs) const {
-  return commandIndex() == rhs.commandIndex() and response() == rhs.response();
+bool EngineReceipt::operator==(ModelType const &rhs) const {
+    if (&rhs == static_cast<ModelType const*>(this)) {
+        return true;
+    }
+
+    return commandIndex() == rhs.commandIndex() &&
+            getTxHash() == rhs.getTxHash() &&
+            getTxIndex() == rhs.getTxIndex() &&
+            getBlockHeight() == rhs.getBlockHeight() &&
+            getBlockHash() == rhs.getBlockHash() &&
+            getFrom() == rhs.getFrom() &&
+            getPayloadType() == rhs.getPayloadType() &&
+            getPayload() == rhs.getPayload() &&
+            getEngineLogs() == rhs.getEngineLogs();
 }
 
 std::string EngineReceipt::toString() const {
   return detail::PrettyStringBuilder()
       .init("EngineReceipt")
       .appendNamed("command_index", commandIndex())
-      .appendNamed("reponse", response())
+      .appendNamed("tx_hash", getTxHash())
+      .appendNamed("tx_index", getTxIndex())
+      .appendNamed("block_height", getBlockHeight())
+      .appendNamed("block_hash", getBlockHash())
+      .appendNamed("from", getFrom())
+      .appendNamed("payload_type", getPayloadType())
+      .appendNamed("payload", getPayload())
+      .appendNamed("engine_logs", getEngineLogs())
       .finalize();
 }
